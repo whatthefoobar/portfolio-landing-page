41a6e25f0cc884b982da501a04c1006a05a1fa09
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2021-10-08 10:54:22 +0200

    adjusted main layout on home

--- js/app.js
+++ js/app.js
@@ -13,9 +13,10 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
 //
-var wave1 = document.querySelector(".wave1path");
-console.log(wave1);
-var wave1Length= wave1.getTotalLength();
-console.log(wave1Length);//2544.73
+// var wave1 = document.querySelector(".wave1path");
+// console.log(wave1);
+// var wave1Length= wave1.getTotalLength();
+// console.log(wave1Length);//2544.73
 
