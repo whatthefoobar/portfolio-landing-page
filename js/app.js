6498ed88ba789e46fc4817e397993407655f6ef0
whatthefoobar <irina.mntn@gmail.com>
2022-04-19 10:59:46 +0200

    changed project section

--- js/app.js
+++ js/app.js
@@ -2,21 +2,37 @@
 
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
 }
 
-//
-// var wave1 = document.querySelector(".wave1path");
-// console.log(wave1);
-// var wave1Length= wave1.getTotalLength();
-// console.log(wave1Length);//2544.73
-
+//card carousel
+$('.slider').owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
